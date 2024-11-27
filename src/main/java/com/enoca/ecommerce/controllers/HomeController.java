c6aed6b9eae416c9f5f04f11c174236f37f6d380
ardaaltinors <ardaaltinors@gmail.com>
2024-11-27 18:02:24 +0300

    Initialize Spring Boot

---
+++ src/main/java/com/enoca/ecommerce/controllers/HomeController.java
@@ -0,0 +1,14 @@
+package com.enoca.ecommerce.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Enoca E-Commerce Backend";
+    }
+
+}
