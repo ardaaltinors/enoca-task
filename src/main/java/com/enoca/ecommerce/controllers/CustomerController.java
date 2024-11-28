34ec337ef4a3b4d41d1b30f1d034c571f409982a
ardaaltinors <ardaaltinors@gmail.com>
2024-11-28 12:37:45 +0300

    Fix exception handling bugs for Customer responses

--- src/main/java/com/enoca/ecommerce/controllers/CustomerController.java
+++ src/main/java/com/enoca/ecommerce/controllers/CustomerController.java
@@ -25,12 +25,8 @@ public class CustomerController {
     // Create a new customer
     @PostMapping
     public ResponseEntity<CustomerResponseDTO> createCustomer(@Valid @RequestBody CustomerRequestDTO customerRequestDTO) {
-        try {
-            CustomerResponseDTO created = customerService.createCustomer(customerRequestDTO);
-            return new ResponseEntity<>(created, HttpStatus.CREATED);
-        } catch (IllegalArgumentException ex) {
-            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
-        }
+        CustomerResponseDTO created = customerService.createCustomer(customerRequestDTO);
+        return new ResponseEntity<>(created, HttpStatus.CREATED);
     }
 
     // Get all customers
@@ -46,34 +42,22 @@ public class CustomerController {
     // Get customer by ID
     @GetMapping("/{id}")
     public ResponseEntity<CustomerResponseDTO> getCustomerById(@PathVariable Long id) {
-        try {
-            CustomerResponseDTO customer = customerService.getCustomerById(id);
-            return new ResponseEntity<>(customer, HttpStatus.OK);
-        } catch (IllegalArgumentException ex) {
-            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
-        }
+        CustomerResponseDTO customer = customerService.getCustomerById(id);
+        return new ResponseEntity<>(customer, HttpStatus.OK);
     }
 
     // Update customer
     @PutMapping("/{id}")
     public ResponseEntity<CustomerResponseDTO> updateCustomer(@PathVariable Long id,
                                                               @Valid @RequestBody CustomerRequestDTO customerRequestDTO) {
-        try {
-            CustomerResponseDTO updated = customerService.updateCustomer(id, customerRequestDTO);
-            return new ResponseEntity<>(updated, HttpStatus.OK);
-        } catch (IllegalArgumentException ex) {
-            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
-        }
+        CustomerResponseDTO updated = customerService.updateCustomer(id, customerRequestDTO);
+        return new ResponseEntity<>(updated, HttpStatus.OK);
     }
 
     // Delete customer
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteCustomer(@PathVariable Long id) {
-        try {
-            customerService.deleteCustomer(id);
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        } catch (IllegalArgumentException ex) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
+        customerService.deleteCustomer(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
