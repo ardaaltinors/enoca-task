cd83f2479ab691620e29423d7c6192835340bad7
ardaaltinors <ardaaltinors@gmail.com>
2024-11-30 15:28:04 +0300

    fix(cart): ensure totalPrice is calculated for cart items

--- src/main/java/com/enoca/ecommerce/mappers/CartItemMapper.java
+++ src/main/java/com/enoca/ecommerce/mappers/CartItemMapper.java
@@ -3,9 +3,12 @@ package com.enoca.ecommerce.mappers;
 import com.enoca.ecommerce.dtos.CartItemRequestDTO;
 import com.enoca.ecommerce.dtos.CartItemResponseDTO;
 import com.enoca.ecommerce.entities.CartItem;
+import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
+
+import java.math.BigDecimal;
 
 @Mapper(componentModel = "spring")
 public interface CartItemMapper {
@@ -17,4 +20,13 @@ public interface CartItemMapper {
     CartItem toEntity(CartItemRequestDTO dto);
 
     void updateEntityFromDto(CartItemRequestDTO dto, @MappingTarget CartItem cartItem);
+
+    @AfterMapping
+    default void calculateTotalPrice(CartItem cartItem, @MappingTarget CartItemResponseDTO responseDTO) {
+        if (cartItem.getPrice() != null && cartItem.getQuantity() != null) {
+            responseDTO.setTotalPrice(cartItem.getPrice().multiply(new BigDecimal(cartItem.getQuantity())));
+        } else {
+            responseDTO.setTotalPrice(BigDecimal.ZERO);
+        }
+    }
 }
